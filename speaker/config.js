341b0a394972654b54b49b25ba070c33f8990d9a
HarpElle <jpaschall@gmail.com>
2026-08-13 03:26:53 +0000

    Add Speaker Portal Pages config (page gate off).
    Required for GitHub Pages; same public-source exposure as the previous HTML-embedded secret.

---
+++ speaker/config.js
@@ -0,0 +1,7 @@
+window.SPEAKER_PORTAL_CONFIG = {
+  googleScriptUrl: 'https://script.google.com/macros/s/AKfycbydn4OwK2ozvtXEKDURSyGQp5Z2TxXHXPl-7AGy5sYPXdr9uAl5BbRI_iokRP4tS1Wv/exec',
+  scriptPassword: 'WWiLgsuUe6RfwtIbe6nnFW2SC5Mr4nKp',
+  pagePassword: '',
+  maxTotalUploadBytes: 20 * 1024 * 1024,
+  fallbackEmail: 'media@anchorfalls.org'
+};
